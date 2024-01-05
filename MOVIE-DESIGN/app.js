13425c0dfe01be47887008640f6ca352e872a57f
MargoCode <143147233+MargoCode@users.noreply.github.com>
2024-01-05 12:39:37 +1000

    movie slider p1
    Nexflix Movie Slider with JavaScript part 1

---
+++ MOVIE-DESIGN/app.js
@@ -0,0 +1,10 @@
+const arrows = document.querySelectorAll(".arrow");
+const movieLists = document.querySelectorAll(".movie-list");
+
+
+arrows.forEach((arrow,i)=>{
+    arrow.addEventListener("click",()=>{
+        movieLists[i].style.transform = "translateX(-100px)"
+    })
+    console.log(movieLists[i].computedStyleMap().get("transform")[0].x.value)
+})
