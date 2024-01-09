87f552f1716f10f211af02f0d7f2585aa5c1aace
yessica-s <153290587+yessica-s@users.noreply.github.com>
2024-01-09 14:17:26 +0000

    dark mode js done

--- MOVIE-DESIGN/app.js
+++ MOVIE-DESIGN/app.js
@@ -19,3 +19,14 @@ arrows.forEach((arrow,i)=>{
 
     console.log(movieLists[i].querySelectorAll("img").length)
 });
+
+const ball = document.querySelector(".toggle-ball");
+const items = document.querySelectorAll(".container, .movie-list-title, .navbar-container, .sidebar, .left-menu-icon, .toggle");
+
+ball.addEventListener("click", ()=>{
+    items.forEach(item=>{
+        item.classList.toggle("active")
+    })
+
+    ball.classList.toggle("active")
+})
